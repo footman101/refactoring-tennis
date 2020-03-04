51281f2aaf8584820cc0622a3822d9c7b885c881
footman101 <houyuxin82@gmail.com>
2020-03-04 15:49:39 +0800

    简化逻辑，内联函数

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -3,6 +3,8 @@ package cn.xpbootcamp.tennis.game1;
 import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame1 implements TennisGame {
+    static final public String[] EVEN_RESULTS = new String[]{"Love-All", "Fifteen-All", "Thirty-All", "Deuce", "Deuce"};
+    static final public String[] SCORE_RESULTS = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
 
     private int player1Score = 0;
     private int player2Score = 0;
@@ -21,72 +23,22 @@ public class TennisGame1 implements TennisGame {
             player2Score += 1;
     }
 
-    private String handleEven() {
-        String result = "";
-        switch (player1Score) {
-            case 0:
-                result = "Love-All";
-                break;
-            case 1:
-                result = "Fifteen-All";
-                break;
-            case 2:
-                result = "Thirty-All";
-                break;
-            default:
-                result = "Deuce";
-                break;
-
-        }
-        return result;
-    }
-
     private String handleAdvantageOrWin() {
-        String result = "";
         int minusResult = player1Score - player2Score;
-        if (minusResult == 1) result = "Advantage " + player1Name;
-        else if (minusResult == -1) result = "Advantage " +  player2Name;
-        else if (minusResult >= 2) result = "Win for " + player1Name;
-        else result = "Win for " + player2Name;
-        return result;
-    }
-
-    private String handleNormal() {
-        String result = "";
-        int tempScore = 0;
-        for (int i = 1; i < 3; i++) {
-            if (i == 1) tempScore = player1Score;
-            else {
-                result += "-";
-                tempScore = player2Score;
-            }
-            switch (tempScore) {
-                case 0:
-                    result += "Love";
-                    break;
-                case 1:
-                    result += "Fifteen";
-                    break;
-                case 2:
-                    result += "Thirty";
-                    break;
-                case 3:
-                    result += "Forty";
-                    break;
-            }
-        }
-        return result;
+        String target = minusResult > 0 ? player1Name : player2Name;
+        String situation = Math.abs(minusResult) == 1 ? "Advantage" : "Win for";
+        return situation + " " + target;
     }
 
     public String getScore() {
         if (player1Score == player2Score) {
-            return handleEven();
+            return EVEN_RESULTS[player1Score];
         }
 
         if (player1Score >= 4 || player2Score >= 4) {
             return handleAdvantageOrWin();
         }
 
-        return handleNormal();
+        return SCORE_RESULTS[player1Score] + "-" + SCORE_RESULTS[player2Score];
     }
 }
